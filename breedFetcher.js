be26dae2859fba167ca4402bc4b813967b2eb5a2
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-26 02:19:13 -0400

    Refactor by moving all the main request logic for fetching breed descriptions from "TheCatsAPI" into a reusable function named "fetchBreedDescription." The refactoring has replaced all the "console.log" calls in the "fetchBreedDescription" function with calls to the callback function. Add code to export the "fetchBreedDescriptions" function at the bottom of the file.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,40 +1,34 @@
 // import "needle" library
 const needle = require('needle');
 
-// get the breed name from the command-line args
-const breedName = process.argv[2];
+// function to fetch breen description
+const fetchBreedDescription = function(breedName, callback) {
+  // API endpoint for TheCatsAPI
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-// case if no name is provided in the command-line
-if (!breedName) {
-  console.log("Please provide a breed name.");
-  process.exit(1);
-}
+  // fetch the data for the breed
+  needle.get(url, (error, response) => {
+    if (error) {
+      callback(error, null); // pass error to callback function
+      return;
+    }
 
-// API endpoint for TheCatsAPI
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+    // the body contains the data sent back from the TheCatsAPI server
+    const body = response.body;
 
-// fetch the data for the breed
-needle.get(url, (error, response) => {
-  if (error) {
-    console.error("Error fetching breed data:", error);
-    return;
-  }
+    // check if the response body is an array with at least one element
+    if (Array.isArray(body) && body.length > 0) {
+      // access the first entry in the data array
+      const breed = body[0];
+      // pass the breed description to the callback function
+      callback(null, breed.description);
+    } else {
+      // edge case if breed is not found, pass null descripton
+      callback(null, null);
+    }
+  });
+};
 
-  // the body contains the data sent back from the TheCatsAPI server
-  const body = response.body;
-  // print out the entire body
-  // console.log("Body:", body);
-  // print out the type of body === object
-  // console.log("Type of body:", typeof body);
 
-  // check if the response body is an array with at least one element
-  if (Array.isArray(body) && body.length > 0) {
-    // access the first entry in the data array
-    const breed = body[0];
-    // print out the breed description for the user
-    console.log("Description:", breed.description);
-  } else {
-    // edge case if breed is not found
-    console.log("Breed not found.");
-  }
-});
+// export fetchBreedDescription function
+module.exports = { fetchBreedDescription };
